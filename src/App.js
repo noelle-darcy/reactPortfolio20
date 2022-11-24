5e62c4a9694922c7b0b7130c6e133289422794eb
Noelle Darcy <noellerdarcy@gmail.com>
2022-11-23 22:40:28 -0500

    finished code

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Body from './components/Body'
 
 function App() {
   return (
     <div>
    <Header></Header>
-
+    <Body></Body>
    <Footer></Footer>
    </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+<section> 
+<div><a href="https://howl-ton.herokuapp.com/">
+        <h2 className="side-titles4">Contact Me</h2>
+      </a><div id="contactMeCard"><a href="https://howl-ton.herokuapp.com/">
+          <u1 className="contact-me">
+          </u1></a><li><a href="https://howl-ton.herokuapp.com/">
+          </a><a href="#number">954.638.0547</a>
+        </li>
+        <li>
+          <a href="mailto: noellerdarcy@gmail.com">noellerdarcy@gmail.com</a>
+        </li>
+        <li>
+          <a href="https://github.com/noelle-darcy">GitHub</a>
+        </li>
+      </div>
+    </div>
+  </section>
