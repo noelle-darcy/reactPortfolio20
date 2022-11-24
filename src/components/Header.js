b675336f70eca255ac3106e799e0f9af66201232
Noelle Darcy <noellerdarcy@gmail.com>
2022-11-23 22:28:12 -0500

    not sure if that worked

---
+++ src/components/Header.js
@@ -0,0 +1,93 @@
+<div>
+  <meta charSet="UTF-8" />
+  <link rel="stylesheet" href="./assets/css/style.css" />
+  <title>Noelle D'Arcy Portfolio</title>
+  <header className="main-header">
+    <h1 id="name">Noelle D'Arcy</h1>
+    <nav>
+      <u1 className="nav-titles">
+        <li>
+          <a href="#about-me">About Me</a>
+        </li>
+        <li>
+          <a href="#work">Work</a>
+        </li>
+        <li>
+          <a href="#resume">Resume</a>
+        </li>
+        <li>
+          <a href="#contact-me">Contact Me</a>
+        </li>
+      </u1>
+    </nav>
+  </header>
+  <section>
+    <div className="header2">
+      <img id="image" src="./assets/images/header image 2.jpg" alt="old fashioned keyboards" />
+      <div className="subtitle">Cool Subtitle</div>
+    </div>
+    {/* This wil be the about me section including a paragraph & a photo of me*/}
+    <div className="about-me">
+      {/*<img src="./assets/images/portfolio-photo.jpg" alt="headshot of Noelle D'Arcy" />*/}
+      <h2 className="side-titles">About Me</h2>
+      <div id="biography">
+        <p>How I got into coding: I started college in 2018 through FAU High's accelerated program which allowed me to be fully enrolled into college when I was 15. During my freshman year of highschool, I joined Science Olympiad to hang out with my friends so much and didn't realize how much it would change my life. 
+          Even though I had applied for science-based competitions, I ended up being assigned to a competition where I had to build a working model helicopter with balsa wood, rubber bands and mylar. Thankfully, my competitive nature set in even though I didn't want to be apart of this competition and I put several hours a night into building, 
+          perfecting and testing my helicopter. On the day of the competition, I was terrified that my hard work wasn't going to pay off. I was so scared that after I launched my helicopter, I instantly closed my eyes. I had no idea what was happening until I heard everyone clapping a minute and 42 seconds later. I finally opened my eyes to see 
+          that everyone had stood up to applaud my build. I had tripled the time of second place as their helicopter only flew for 38 seconds. This was the first time they hadn't placed first in over 7 years and I blew them out of the water. Due to my dedication to the club, I was promoted to head captain for the following year. Without even knowing it, 
+          such a little thing had completely changed my life. Soon after, other organizations asked for my help on their builds too which is how I became part of American Society of Civil Engineers. </p><p>I was a Chemistry major that was head captain of Science Olympiad and Secretary of ASCE. Then, I became the treasurer of American Society of Mechanical Engineers 
+          and then the president of ASCE. Then, I had the most amazing opportunity and got to be the buisness head of Solar Owls, an organization that was competiting in a national competition with a fully solar powered SUV. After 2 years of being a Chemistry major, I finally switched over to Mechanical Engineering. I went to tons of professional events, got 
+          offered several internships and scholarships before I had even turnt 18. I wasn't fully convinced engineering was what I wanted to do in the future but I assumed there was no way I could know what I wanted to do for the rest of my life considering I wasn't even an adult yet. Then, one day, my vice president of ASCE came up to me and said he couldn't 
+          find anyone who was willing to take on the coding competition and there was only 2 weeks until the competition. It wasn't required that we competeted in this competition but I knew that I didn't just spend the last year working on a club that had 5 members into one with over 100, just to not compete with full force. Without having ever even seen code 
+          before, I decided I was going to take on the competition and I was going to prove how much my club had improved. I had 2 weeks to learn everything about C++ even though I had 7 other competitions that were all happening in that month. I was on the plane to competitions praticing code, I was watching videos while building towers, I was staying up all 
+          night trying to do the pratice codes that they had provided us. I finally got to the competition and realized that even though I had two other builds competiting, the one I was most excited for was my coding competition. They ended up cancelling the competition, the night before, due to COVID. I was devestated and left coding alone for a while since 
+          I had to handle the precussions of the competition being cancelled. I had an empty class spot about a year later and I decided I would do a coding class for fun. Once again, I realized how much I loved coding, how natural it felt to me, how I could get so carried away with it that I'd forget about everything around me. For another year, I continued to 
+          stay in Mechanical Engineering. I had already switched my major and I had so many amazing opportunities lined up for me once I graduated and I didn't want all of my hard work and dedication to those clubs to just be wasted. Then I realized that I had just turned 19 and I still had my entire life ahead of me so I should go after what I wanted and not just
+          what was easier. That's when I decided I was going to switch my major again even though everyone protested it. </p> </div>
+    </div>
+    {/* This will be the work section which will display your completed projects. */}
+    <div className="work">
+      <h2 className="side-titles2">Work</h2>
+    </div>
+    <div id="Work">
+      <div id="card1">
+        <a href="https://howl-ton.herokuapp.com/">
+          <img alt="Qries" src="https://i.ibb.co/60vGBqG/project2-headericon.jpg" width={400} height={300} />
+        </a></div><a href="https://howl-ton.herokuapp.com/">
+      </a><div id="card2"><a href="https://howl-ton.herokuapp.com/">
+        </a><a href="https://howl-ton.herokuapp.com/">
+          <img alt="Qries" src="https://i.ibb.co/60vGBqG/project2-headericon.jpg" width={400} height={300} />
+        </a></div><a href="https://howl-ton.herokuapp.com/">
+      </a><div id="card3"><a href="https://howl-ton.herokuapp.com/">
+        </a><a href="https://howl-ton.herokuapp.com/">
+          <img alt="Qries" src="https://i.ibb.co/60vGBqG/project2-headericon.jpg" width={400} height={300} />
+        </a></div><a href="https://howl-ton.herokuapp.com/">
+      </a><div id="card4"><a href="https://howl-ton.herokuapp.com/">
+        </a><a href="https://howl-ton.herokuapp.com/">
+          <img alt="Qries" src="https://i.ibb.co/60vGBqG/project2-headericon.jpg" width={400} height={300} />
+        </a></div><a href="https://howl-ton.herokuapp.com/">
+      </a></div><a href="https://howl-ton.herokuapp.com/">
+      {/* This will include your resume: certificates, education, achivements, skills, etc. */}
+      {/* <div class="resume">
+      <h2 class="side-titles3">Resume</h2>
+  </div> */}
+      {/* This will be the contact information including: phone number, email, website, GitHub */}
+    </a><div><a href="https://howl-ton.herokuapp.com/">
+        <h2 className="side-titles4">Contact Me</h2>
+      </a><div id="contactMeCard"><a href="https://howl-ton.herokuapp.com/">
+          <u1 className="contact-me">
+          </u1></a><li><a href="https://howl-ton.herokuapp.com/">
+          </a><a href="#number">954.638.0547</a>
+        </li>
+        <li>
+          <a href="mailto: noellerdarcy@gmail.com">noellerdarcy@gmail.com</a>
+        </li>
+        <li>
+          <a href="https://github.com/noelle-darcy">GitHub</a>
+        </li>
+      </div>
+    </div>
+  </section>
+</div>
+
+
